6b9aadc1d99df3209773eb88feac076cee251f01
manthan39 <30994648+manthan39@users.noreply.github.com>
2021-02-11 14:37:14 +0530

    Update whatappSendMessage.py

--- whatappSendMessage.py
+++ whatappSendMessage.py
@@ -1,28 +1,22 @@
-# Reading an excel file using Python
-import xlrd
+import pywhatkit
 from datetime import date
 today = date.today()
+import time
+import xlrd
+from datetime import date
+
+today = date.today()
+t=time.localtime()
+loc = ("birthdaylist.xlsx")
 
 
-# Give the location of the file
-loc = ("C:\\Users\\mvbhatiya\\Desktop\\birthdaylist.xlsx")
-
-# To open Workbook
 wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0)
 
-# For row 0 and column 0
-# print(sheet.cell_value(0, 0))
-# print(sheet.nrows)
 curdate=(str(today.day)+"-"+str(today.month))
 for i in range(sheet.nrows):
     if curdate==sheet.cell_value(i, 1):
-    	name=(sheet.cell_value(i,0))
+    	number=(sheet.cell_value(i,0))
     	message=(sheet.cell_value(i,2))
-
-
-print(name)
-print(message)
-    	
-
+    	pywhatkit.sendwhatmsg(number,message,int(time.strftime("%H",t)),int(time.strftime("%M",t))+2)
 
